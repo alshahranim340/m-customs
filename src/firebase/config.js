020d76a04b5183a95242fa7a01e9ca624651a4d4
alshahranim340 <alshahranim340@gmail.com>
2026-09-02 11:23:51 +0300

    02/09

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -4,17 +4,17 @@ import { getStorage } from "firebase/storage";
 import { getAnalytics } from "firebase/analytics";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCUldI4aarUZBJ7627uUjCHsGPlqfekgfA",
-  authDomain: "m-customs.firebaseapp.com",
-  projectId: "m-customs",
-  storageBucket: "m-customs.firebasestorage.app",
-  messagingSenderId: "869699243478",
-  appId: "1:869699243478:web:0ec5f8199f3d8604692877",
-  measurementId: "G-XCTNG77EXJ"
+  apiKey:            import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain:        import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId:         import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket:     import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId:             import.meta.env.VITE_FIREBASE_APP_ID,
+  measurementId:     import.meta.env.VITE_FIREBASE_MEASUREMENT_ID,
 };
 
 const app = initializeApp(firebaseConfig);
-export const db = getFirestore(app);
-export const storage = getStorage(app);
+export const db        = getFirestore(app);
+export const storage   = getStorage(app);
 export const analytics = getAnalytics(app);
 export default app;
